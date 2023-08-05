5a4d59ff621b547581d26670532bbd9ee69908fe
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-08-05 20:47:02 -0300

    fix: corrigindo chackra com next

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { ChakraProvider } from "@chakra-ui/react"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        <ChakraProvider>
+          <div>
+            {children}
+          </div>
+        </ChakraProvider>
+      </body>
+    </html>
+  )
+}
