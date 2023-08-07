8fc74ad238a280dd6a4216569082da2cde462571
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-08-07 18:16:39 -0300

    feat: tela visão geral do usuario

--- frontend/app/user/dashboard/page.tsx
+++ frontend/app/user/dashboard/page.tsx
@@ -1,14 +1,107 @@
 "use client"
 
+import { useEffect, useState } from 'react';
+import { Box, Table, Tbody, Td, Th, Thead, Tr, Button, Spinner, Center } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { userService } from '@/api/userAPI';
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
+
+interface User {
+  id: number;
+  email: string;
+  name: string;
+  hireDate: string;
+  vacations: Vacation[];
+}
+
+interface Vacation {
+  id: number;
+  startVacation: string;
+  endVacation: string;
+}
+
+interface Token {
+  id: number;
+  iat: number;
+  exp: number;
+}
+
 export default function UserDashboard() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const token: any = window.localStorage.getItem('token');
+  useEffect(() => {
+  const decodedToken: any = jwt.decode(token);
+    const fetchData = async () => {
+      try {
+        const data = await userService.getUser(decodedToken.id);
+        setUser(data);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [token]);
+
+  if (loading) {
+    return (
+      <Center>
+        <Spinner />
+      </Center>
+    );
+  }
+
+  if (!user) {
+    return (
+      <Box>
+        <h1>Não foi possível carregar os dados do usuário</h1>
+      </Box>
+    );
+  }
+
   return (
     <>
-      <h1>Página de visão geral do usuario</h1>
-      <p>Essa pagina terá as rotas GET /users/:id, onde o id virá do token</p>
-      <p>haverá um forms para cadastro de novas férias</p>
-      <p>caso haja férias cadastradas os dados serão exibidos numa tabela, que terá botões para editar/excluir/expandir</p>
-      <p>cada botão clicado fará uma requisição diferente a api</p>
-      <p>essa rota será autenticada onde apenas o token de rh/usuário pode acessar</p>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Nome</Th>
+              <Th>Email</Th>
+              <Th>Data de Contratação</Th>
+              <Th>Início das Férias</Th>
+              <Th>Final das Férias</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>{user.name}</Td>
+              <Td>{user.email}</Td>
+              <Td>{moment(user.hireDate).format('DD/MM/YYYY')}</Td>
+              <Td>
+                {user.vacations.length > 0
+                  ? moment(user.vacations[user.vacations.length - 1].startVacation).format('DD/MM/YYYY')
+                  : '-'}
+              </Td>
+              <Td>
+                {user.vacations.length > 0
+                  ? moment(user.vacations[user.vacations.length - 1].endVacation).format('DD/MM/YYYY')
+                  : '-'}
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </Box>
+      {user.vacations.length === 0 && (
+        <Box marginTop="10">
+          <Button leftIcon={<AddIcon />} colorScheme="teal" variant="solid" onClick={() => window.location.href='/user/vacation'}>
+            Registrar Novas Férias
+          </Button>
+        </Box>
+      )}
     </>
-  )
+  );
 }
